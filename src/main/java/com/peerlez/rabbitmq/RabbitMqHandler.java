e38214feb13ccb3f8cbea2d6620763065f83c6a3
Antti Sillanpää <peerlez@gmail.com>
2015-01-27 10:05:51 +0200

    Removed throws RabbitMqException lines no need to throw any exception.

--- src/main/java/com/peerlez/rabbitmq/RabbitMqHandler.java
+++ src/main/java/com/peerlez/rabbitmq/RabbitMqHandler.java
@@ -6,31 +6,29 @@ import com.rabbitmq.client.Channel;
  * @author A.Sillanpaa
  *
  */
-public interface RabbitMqMessage {
+public interface RabbitMqHandler {
 
 	/**
 	 * Publish messages to queues.
 	 *
 	 * @param message message entity to publish {@link Message}
 	 * 
-	 * @throws RabbitMqClientException
 	 */
-	void publish(Message message, RabbitMqConnection connection) 
-			throws RabbitMqClientException;
+	void publish(Message message, RabbitMqConnection connection);
 	
 	/**
 	 * Registers and initializes consumer to consume given Broker(exchange).
 	 *
-	 * @param exchangeName the exhange name to use on message subscribe.
-	 * @param routingKey the routing key to use for the binding to queue.
+	 * @param exchangeName 
+	 * 				the exhange name to use on message subscribe.
+	 * @param routingKey 
+	 * 				the routing key to use for the binding to queue.
 	 *
 	 * @return new {@link Channel} which is bind to an exchange by the given
-	 *         parameters
+	 *         		parameters
 	 *
-	 * @throws RabbitMqClientException if something went wrong on Consumer
-	 * 		   initialization
 	 */
 	Channel subscribe(String exchangeName, String routingKey, 
-			RabbitMqConnection connection) throws RabbitMqClientException;
+			RabbitMqConnection connection);
 
 }
